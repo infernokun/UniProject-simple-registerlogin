7957a3c43e43e554647220cbf67a1e14635fb5c6
Javoris Brown <sasukeucheha@live.com>
2022-11-14 20:08:39 -0600

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_1a36e5c5f5ecb77';
-    $username = 'b14ae626e4c908';
-    $password = '853192af';
+    $dsn = '?';
+    $username = '?';
+    $password = '?';
 
     try {
         $db = new PDO($dsn, $username, $password);
